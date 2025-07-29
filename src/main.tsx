89d268ef104ec8e3b1098e8aa7482467e23acaf4
joan purba <joanpurba562@gmail.com>
2025-07-29 20:27:59 +0700

    feat: add quiz page

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
+const Quiz = lazy(() => import("./pages/Quiz"));
 
 const router = createBrowserRouter([
 	{
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
 	{
 		path: "/daftar",
 		element: <Register />,
+	},
+	{
+		path: "/quiz",
+		element: <Quiz />,
 	},
 ]);
 
